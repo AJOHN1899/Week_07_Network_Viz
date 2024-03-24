75096b36ee8a4dbe18c0823889717104811a80ab
AJOHN1899 <ajohn1899@gmail.com>
2024-03-24 01:09:56 -0400

    Week_07_Version_1
    Week_07_Network Assignment

--- network.js
+++ network.js
@@ -1,44 +1,100 @@
 function simulate(data,svg)
 
 {
-    let width = parseInt(svg.attr("viewBox").split(' ')[2])
-    let height = parseInt(svg.attr("viewBox").split(' ')[3])
-    let color = d3.scaleOrdinal(d3.schemeCategory10);
-    let link_elements = svg.append("g")
-        .attr('transform',`translate(${width/2},${height/2})`)
+    const width = parseInt(svg.attr("viewBox").split(' ')[2])
+    const height = parseInt(svg.attr("viewBox").split(' ')[3])
+    const main_group = svg.append("g")
+                    .attr("class", "canvas");
+                    //.attr("transform", "translate(0, 50)")
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    //let color = d3.scaleOrdinal(d3.schemePaired);
+
+        //calculate degree of the nodes:
+        let node_degree={}; //initiate an object
+        data.nodes.forEach((d, i)=>node_degree[i]=0)
+        data.links.forEach(
+         (d)=>{
+            if(d.source in node_degree)
+            {
+                node_degree[d.source]++
+            }
+            else{
+                node_degree[d.source]=0
+            }
+            if(d.target in node_degree)
+            {
+                node_degree[d.target]++
+            }
+            else{
+                node_degree[d.target]=0
+            }
+          }
+        )
+    
+        const degreeExtent = d3.extent(Object.values(node_degree), d=>d)
+       
+        const scale_radius = d3.scaleSqrt()
+            .domain(degreeExtent)
+            .range([5,30])
+
+        const scale_link_stroke_width = d3.scaleLinear()
+            .domain(d3.extent(data.links, d=> d.id))
+            .range([1,5])
+
+        const link_elements = main_group.append("g")
+        .attr('transform',`translate(${width / 2},${height / 2})`)
         .selectAll(".line")
         .data(data.links)
         .enter()
         .append("line")
-        .style("stroke-width", d=> d.value);
-    let node_elements = svg.append("g")
+        .attr("stroke", "black")
+        //.style("stroke-width", d=> scale_link_stroke_width(d.value))
+     
+        
+        const node_elements = main_group.append("g")
         .attr('transform', `translate(${width / 2},${height / 2})`)
         .selectAll(".circle")
         .data(data.nodes)
         .enter()
-        .append("circle")
-        .attr("r", 5)
-        .attr("fill", d=>color(d.group))
+        .append("g")
+        
+        node_elements.append("circle")
+        .attr("r", function(d) {return d.degree})
+        .attr("fill", d=>color(d.cluster_label)) 
+        .attr("class", function (d){return "gr_"+d.cluster_label})
+            .on("mouseenter", function (m,d){
+                d3.selectAll("circle").classed("inActive", true)
+                d3.selectAll(".gr_"+d.cluster_label).classed("inActive", false)
+               // d3.selectAll(".gr_" +d.cluster_label.classed("inActive", false))
+        }) 
+            .on("mouseleave", (m,d)=>{
+                d3.selectAll("circle").classed("inActive", false)
+        })
 
-    let ForceSimulation = d3.forceSimulation(data.nodes)
-        .force("collide", d3.forceCollide().radius(15))
+        node_elements.append("text")
+        //.attr("class","label")
+        .attr("text-anchor","middle")
+        .text(d=>d.id)
+
+        node_elements.on("mouseenter", (m,d)=>{
+            console.log(d)
+        })
+
+    const ForceSimulation = d3.forceSimulation(data.nodes)
+        .force("collide", d3.forceCollide().radius(35).iterations(1))
         .force("x", d3.forceX())
         .force("y", d3.forceY())
-        .force("charge", d3.forceManyBody())
-        //.force("link",d3.forceLink(data.links)) // we add links data to this layout
-        .force("link",d3.forceLink(data.links)
-            .id(d=>d.index)
-            .distance(d=>d.value)
-            .strength(d=>d.value*.1)
-        )
-
+        .force("charge", d3.forceManyBody().strength(-10))
+        .force('link', d3.forceLink(data.links)
+                         .id(function(n) {return n.id; }))
+                  
         .on("tick", ticked);
+
 
     function ticked()
     {
-    node_elements
-            .attr("cx", d=> d.x)
-            .attr("cy", d=> d.y)
+        node_elements
+            .attr('transform', d=> {return `translate(${d.x},${d.y})`})
         link_elements
             .attr("x1",d=>d.source.x)
             .attr("x2",d=>d.target.x)
@@ -46,4 +102,13 @@ function simulate(data,svg)
             .attr("y2",d=>d.target.y)
 
         }
+
+
+    svg.call(d3.zoom()
+        .extent([[0, 0], [width, height]])
+        .scaleExtent([1, 8])
+        .on("zoom", zoomed));
+    function zoomed({transform}) {
+        main_group.attr("transform", transform);
+    }
 }
